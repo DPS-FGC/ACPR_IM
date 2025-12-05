e5f547ad2b8a824233e609d42199e952d516c557
DPS-FGC <dpsfgc001@gmail.com>
2025-12-05 11:33:17 +0200

    Fix crash when loading EX ky (Issue #8).

--- src/Game/Hitbox/HitBoxReader.cpp
+++ src/Game/Hitbox/HitBoxReader.cpp
@@ -10,6 +10,9 @@ std::vector<Hitbox> HitboxReader::getHitboxes(const CharData* charObj)
 	const int entriesCount = charObj->numHitbox;
 	Hitbox* pEntry = reinterpret_cast<Hitbox*>(charObj->hitboxArray);
 
+	if (pEntry == NULL) //Fixes crash when loading EX ky
+		return hitboxes;
+
 	for (int i = 0; i < entriesCount; i++)
 	{
 		hitboxes.push_back(*pEntry);
